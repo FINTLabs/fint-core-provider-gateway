345089e5e64ced0e8002f085935fd1ff578b9412
Trond Magnus Sevre <trond@fintlabs.no>
2022-11-04 11:00:00 +0100

    Add restcontroller for events

---
+++ src/main/java/no/fintlabs/events/downstream/EventController.java
@@ -0,0 +1,25 @@
+package no.fintlabs.events.downstream;
+
+import lombok.RequiredArgsConstructor;
+import no.fintlabs.adapter.models.RequestFintEventCastable;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+@RestController(value = "/event")
+public class EventController {
+
+    private final EventService eventService;
+
+    @GetMapping
+    public List<RequestFintEventCastable> getEvents(
+            @PathVariable(required = false) String domainName,
+            @PathVariable(required = false) String packageName,
+            @PathVariable(required = false) String resourceName
+    ) {
+        return eventService.getEvents(domainName, packageName, resourceName);
+    }
+}
--- src/main/java/no/fintlabs/events/downstream/EventService.java
+++ src/main/java/no/fintlabs/events/downstream/EventService.java
@@ -1,30 +1,31 @@
-package no.fintlabs.requests;
+package no.fintlabs.events.downstream;
 
-import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import no.fintlabs.adapter.models.RequestFintEventCastable;
 import no.fintlabs.kafka.common.topic.pattern.FormattedTopicComponentPattern;
 import no.fintlabs.kafka.common.topic.pattern.ValidatedTopicComponentPattern;
 import no.fintlabs.kafka.event.EventConsumerFactoryService;
 import no.fintlabs.kafka.event.topic.EventTopicNamePatternParameters;
+import org.apache.commons.lang3.StringUtils;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.PostConstruct;
 import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Slf4j
 @Service
-@RequiredArgsConstructor
-public class RequestsService {
+public class EventService {
 
     private final EventConsumerFactoryService eventConsumerFactoryService;
 
-    private ArrayList<RequestFintEventCastable> requests;
+    private final ArrayList<RequestFintEventCastable> events;
 
-    public RequestsService(EventConsumerFactoryService eventConsumerFactoryService) {
+    public EventService(EventConsumerFactoryService eventConsumerFactoryService) {
         this.eventConsumerFactoryService = eventConsumerFactoryService;
-        requests = new ArrayList<>();
+        events = new ArrayList<>();
     }
 
     @PostConstruct
@@ -35,7 +36,7 @@ public class RequestsService {
                 .builder()
                 .orgId(FormattedTopicComponentPattern.any())
                 .domainContext(FormattedTopicComponentPattern.anyOf("fint-core"))  // Optional if set as application property
-                .eventName(ValidatedTopicComponentPattern.endingWith( "-request"))
+                .eventName(ValidatedTopicComponentPattern.endingWith("-request"))
                 .build();
 
         eventConsumerFactoryService.createFactory(
@@ -46,6 +47,15 @@ public class RequestsService {
 
     private void processEvent(ConsumerRecord<String, RequestFintEventCastable> consumerRecord) {
         log.info("You got a " + consumerRecord.value().getValue().getClass().getName());
-        requests.add(consumerRecord.value());
+        events.add(consumerRecord.value());
+    }
+
+    public List<RequestFintEventCastable> getEvents(String domainName, String packageName, String resourceName){
+        return events
+                .stream()
+                .filter(events -> StringUtils.isBlank(domainName) || events.getDomainName().equalsIgnoreCase(domainName))
+                .filter(events -> StringUtils.isBlank(packageName) || events.getPackageName().equalsIgnoreCase(packageName))
+                .filter(events -> StringUtils.isBlank(resourceName) || events.getPackageName().equalsIgnoreCase(resourceName))
+                .collect(Collectors.toList());
     }
 }
