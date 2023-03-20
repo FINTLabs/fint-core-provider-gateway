f34d9159ee869e47cd7e085382424633478d02fa
Henrik Knutsen <hknots99@gmail.com>
2023-03-20 10:24:30 +0100

    add event-corrid to entity header (#19)

--- src/main/java/no/fintlabs/event/response/ResponseEventService.java
+++ src/main/java/no/fintlabs/event/response/ResponseEventService.java
@@ -43,7 +43,8 @@ public class ResponseEventService {
                 requestEvent.getDomainName(),
                 requestEvent.getPackageName(),
                 requestEvent.getResourceName(),
-                responseFintEvent.getValue()
+                responseFintEvent.getValue(),
+                responseFintEvent.getCorrId()
         );
     }
 }
