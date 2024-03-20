82824f014c95204324d139546bf34cd9b20adf7e
Trond Magnus Sevre <trond@fintlabs.no>
2024-03-20 15:45:09 +0100

    Remove old removed-events

--- src/main/java/no/fintlabs/event/request/RequestEventService.java
+++ src/main/java/no/fintlabs/event/request/RequestEventService.java
@@ -3,8 +3,10 @@ package no.fintlabs.event.request;
 import lombok.extern.slf4j.Slf4j;
 import no.fintlabs.adapter.models.RequestFintEvent;
 import org.apache.commons.lang3.StringUtils;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDate;
 import java.util.*;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
@@ -13,15 +15,28 @@ import java.util.stream.Stream;
 @Service
 public class RequestEventService {
 
+    public static final int DAYS_TO_KEEP_REMOVED_EVENTS = 2;
     private final Map<String, RequestFintEvent> events = new LinkedHashMap<>();
+    private final Map<String, LocalDate> removedEvents = new HashMap<>();
 
     public void addEvent(RequestFintEvent event) {
-        events.put(event.getCorrId(), event);
+        if (removedEvents.containsKey(event.getCorrId())) {
+            log.debug("Event with corrId: {} not added because in removed events", event.getCorrId());
+        } else {
+            events.put(event.getCorrId(), event);
+            log.debug("Event with corrId: {} added", event.getCorrId());
+        }
     }
 
     public void removeEvent(String corrId) {
-        Optional<RequestFintEvent> request = getEvent(corrId);
-        request.ifPresent(events::remove);
+        removedEvents.put(corrId, LocalDate.now());
+
+        if (events.containsKey(corrId)) {
+            events.remove(corrId);
+            log.debug("Event with corrId: {} removed", corrId);
+        } else {
+            log.warn("Failed to remove event with corrId: {}", corrId);
+        }
     }
 
     public List<RequestFintEvent> getEvents(String orgId, String domainName, String packageName, String resourceName, int size) {
@@ -38,5 +53,11 @@ public class RequestEventService {
 
     public Optional<RequestFintEvent> getEvent(String corrId) {
         return Optional.ofNullable(events.get(corrId));
+    }
+
+    @Scheduled(cron = "0 0 10,15 * * ?")
+    private void removeOldEvents() {
+        LocalDate now = LocalDate.now();
+        removedEvents.entrySet().removeIf(entry -> entry.getValue().isBefore(now.minusDays(DAYS_TO_KEEP_REMOVED_EVENTS)));
     }
 }
