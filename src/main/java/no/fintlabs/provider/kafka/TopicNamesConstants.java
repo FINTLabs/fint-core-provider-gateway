d532dbc946cb4b5464452d49b48b428acd87a28c
Hknots <hknots99@gmail.com>
2024-08-27 23:53:43 +0200

    add fint-core to constants

--- src/main/java/no/fintlabs/provider/kafka/TopicNamesConstants.java
+++ src/main/java/no/fintlabs/provider/kafka/TopicNamesConstants.java
@@ -2,6 +2,8 @@ package no.fintlabs.provider.kafka;
 
 public class TopicNamesConstants {
 
+    public static String FINT_CORE = "fint-core";
+
     public static String HEARTBEAT_EVENT_NAME = "adapter-health";
     public static String ADAPTER_REGISTER_EVENT_NAME = "adapter-register";
     public static String ADAPTER_FULL_SYNC_EVENT_NAME = "adapter-full-sync";
