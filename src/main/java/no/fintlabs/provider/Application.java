c990ca70b716a11a30fa6bc56440d2634be82a7e
Hknots <hknots99@gmail.com>
2024-08-27 07:37:08 +0200

    componentscan entire no.fintlabs

--- src/main/java/no/fintlabs/provider/Application.java
+++ src/main/java/no/fintlabs/provider/Application.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
-@ComponentScan("no.fintlabs.kafka")
+@ComponentScan("no.fintlabs")
 @SpringBootApplication
 public class Application {
 
