241db9fe96ae42a2b0ed46329564a2b2ea35cf61
Hknots <hknots99@gmail.com>
2024-08-30 15:10:35 +0200

    add capability validation to AdapterContractContext

--- src/main/java/no/fintlabs/provider/security/AdapterContractContext.java
+++ src/main/java/no/fintlabs/provider/security/AdapterContractContext.java
@@ -4,15 +4,21 @@ import no.fintlabs.adapter.models.AdapterContract;
 import org.springframework.stereotype.Component;
 
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Map;
+import java.util.Set;
 
 @Component
 public class AdapterContractContext {
 
     private final Map<String, String> adapterIdToUsernameMap = new HashMap<>();
+    private final Map<String, Set<String>> adapterIdValidCapabilities = new HashMap<>();
 
-    public void add(String adapterId, String username) {
-        adapterIdToUsernameMap.put(adapterId, username);
+    public boolean adapterCanPerformCapability(String adapterId, String domainName, String packageName, String entityName) {
+        String compatibilityLink = "%s/%s/%s".formatted(domainName, packageName, entityName).toLowerCase();
+        return adapterIdValidCapabilities.get(adapterId).contains(compatibilityLink);
+    }
+
     public void add(AdapterContract adapterContract) {
         adapterIdToUsernameMap.put(adapterContract.getAdapterId(), adapterContract.getUsername());
         addAdapterIdValidCapabilities(adapterContract);
@@ -20,5 +26,17 @@ public class AdapterContractContext {
 
     public boolean userCanAccessAdapter(String username, String adapterId) {
         return adapterIdToUsernameMap.getOrDefault(adapterId, "").equals(username);
+    }
+
+    private void addAdapterIdValidCapabilities(AdapterContract adapterContract) {
+        adapterIdValidCapabilities.put(adapterContract.getAdapterId(), new HashSet<>());
+        adapterContract.getCapabilities().forEach(capability ->
+                adapterIdValidCapabilities.get(adapterContract.getAdapterId()).add(
+                        "%s/%s/%s".formatted(
+                                        capability.getDomainName(),
+                                        capability.getPackageName(),
+                                        capability.getResourceName())
+                                .toLowerCase()
+                ));
     }
 }
