346cfbf803a4af3edf22d721255a949c94c1c05f
Hknots <hknots99@gmail.com>
2024-09-20 15:13:30 +0200

    add metamodel webclient config

--- src/main/java/no/fintlabs/provider/config/WebFluxConfig.java
+++ src/main/java/no/fintlabs/provider/config/WebFluxConfig.java
@@ -1,12 +1,23 @@
 package no.fintlabs.provider.config;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.codec.ServerCodecConfigurer;
 import org.springframework.web.reactive.config.PathMatchConfigurer;
 import org.springframework.web.reactive.config.WebFluxConfigurer;
+import org.springframework.web.reactive.function.client.WebClient;
 
 @Configuration
 public class WebFluxConfig implements WebFluxConfigurer {
+
+    @Value("${fint.metamodel.base-url}")
+    private String metamodelUrl;
+
+    @Bean
+    public WebClient webhookWebClient() {
+        return WebClient.builder().baseUrl(metamodelUrl).build();
+    }
 
     @Override
     public void configureHttpMessageCodecs(ServerCodecConfigurer configurer) {
