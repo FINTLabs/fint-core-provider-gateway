2b1bf470c6396c21bc99da7ee435d40d6857d209
Hknots <hknots99@gmail.com>
2024-11-06 10:02:00 +0100

    add new exception

---
+++ src/main/java/no/fintlabs/provider/exception/InvalidSyncPageEntryException.java
@@ -0,0 +1,7 @@
+package no.fintlabs.provider.exception;
+
+public class InvalidSyncPageEntryException extends RuntimeException {
+    public InvalidSyncPageEntryException(String message) {
+        super(message);
+    }
+}
