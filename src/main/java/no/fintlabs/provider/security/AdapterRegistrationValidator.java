bc735ce481d03a8ac891d943a0920d074fbe0aa5
Trond Magnus Sevre <76692689+trondsevre@users.noreply.github.com>
2024-11-11 11:56:42 +0100

    Improve logging and make capabilities lowercase (#26)

--- src/main/java/no/fintlabs/provider/security/AdapterRegistrationValidator.java
+++ src/main/java/no/fintlabs/provider/security/AdapterRegistrationValidator.java
@@ -1,6 +1,7 @@
 package no.fintlabs.provider.security;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import no.fintlabs.adapter.models.AdapterCapability;
 import no.fintlabs.provider.exception.InvalidAdapterCapabilityException;
 import no.fintlabs.provider.security.resource.ResourceContext;
@@ -8,6 +9,7 @@ import org.springframework.stereotype.Component;
 
 import java.util.Set;
 
+@Slf4j
 @Component
 @RequiredArgsConstructor
 public class AdapterRegistrationValidator {
@@ -16,8 +18,9 @@ public class AdapterRegistrationValidator {
 
     public void validateCapabilities(Set<AdapterCapability> capabilities) {
         capabilities.forEach(capability -> {
-            String componentResource = "%s-%s-%s".formatted(capability.getDomainName(), capability.getPackageName(), capability.getResourceName());
+            String componentResource = "%s-%s-%s".formatted(capability.getDomainName(), capability.getPackageName(), capability.getResourceName()).toLowerCase();
             if (!resourceContext.getValidResources().contains(componentResource)) {
+                log.warn("Validation failed: Capability '{}' from '{}' is not a valid resource.", capability, componentResource);
                 throw new InvalidAdapterCapabilityException("Invalid capability resource: %s".formatted(componentResource));
             }
         });
