4d1885df3c15d7feced6803b61e3b3c5ae6ffe05
Hknots <hknots99@gmail.com>
2024-12-16 14:02:58 +0100

    add AdapterNotRegisteredException

---
+++ src/main/java/no/fintlabs/provider/exception/AdapterNotRegisteredException.java
@@ -0,0 +1,7 @@
+package no.fintlabs.provider.exception;
+
+public class AdapterNotRegisteredException extends RuntimeException {
+    public AdapterNotRegisteredException(String message) {
+        super(message);
+    }
+}
