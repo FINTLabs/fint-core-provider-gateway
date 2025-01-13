36fcff01c20fabeb8cf2cc9694a0ea177d659d13
Hknots <hknots99@gmail.com>
2025-01-13 09:20:48 +0100

    create InvalidResponseFintEventException

---
+++ src/main/java/no/fintlabs/provider/exception/InvalidResponseFintEventException.java
@@ -0,0 +1,7 @@
+package no.fintlabs.provider.exception;
+
+public class InvalidResponseFintEventException extends RuntimeException {
+    public InvalidResponseFintEventException(String message) {
+        super(message);
+    }
+}
