72132870c9bcc4b249af11bb4dce331cdb783bab
Hknots <hknots99@gmail.com>
2025-11-25 09:11:54 +0100

    refactor: migrate ResourceContext from Java to Kotlin and use metamodel to create valid resources

--- src/main/java/no/fintlabs/provider/security/resource/ResourceContext.kt
+++ src/main/java/no/fintlabs/provider/security/resource/ResourceContext.kt
@@ -1,34 +1,21 @@
-package no.fintlabs.provider.security.resource;
+package no.fintlabs.provider.security.resource
 
-import lombok.Getter;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.stereotype.Component;
-import org.springframework.web.reactive.function.client.WebClient;
-
-import java.util.Set;
-import java.util.stream.Collectors;
+import lombok.Getter
+import no.fintlabs.metamodel.MetamodelService
+import org.springframework.stereotype.Component
 
 @Getter
 @Component
-public class ResourceContext {
+class ResourceContext(
+    metamodelService: MetamodelService
+) {
+    val validResources: Set<String> = createValidResources(metamodelService)
 
-    private final Set<String> validResources;
+    private fun createValidResources(metamodelService: MetamodelService) =
+        metamodelService.getComponents().flatMap { component ->
+            component.resources.map { resource ->
+                "${component.domainName}-${component.packageName}-${resource.name}"
+            }
+        }.toSet()
 
-    public ResourceContext(@Value("${fint.metamodel.base-url}") String metamodelBaseUrl) {
-        validResources = fetchValidResources(WebClient.builder().baseUrl(metamodelBaseUrl).build());
-    }
-
-    private Set<String> fetchValidResources(WebClient webClient) {
-        return webClient.get()
-                .retrieve()
-                .bodyToFlux(ResourceMetadata.class)
-                .map(resourceMetadata ->
-                        String.format("%s-%s-%s",
-                                resourceMetadata.domainName(),
-                                resourceMetadata.packageName(),
-                                resourceMetadata.resourceName())
-                )
-                .collect(Collectors.toSet())
-                .block();
-    }
 }
