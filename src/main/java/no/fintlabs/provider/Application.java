55665bf85b09da100ffcf91293bff59b047863f9
Hknots <hknots99@gmail.com>
2026-02-19 13:55:22 +0100

    feat: scan no.novari package

--- src/main/java/no/fintlabs/provider/Application.java
+++ src/main/java/no/fintlabs/provider/Application.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
-@ComponentScan("no.fintlabs")
+@ComponentScan(basePackages = {"no.fintlabs", "no.novari"})
 @SpringBootApplication
 public class Application {
 
