6b850edab3e8139c197d1fd53307735b2e2858f5
Hknots <hknots99@gmail.com>
2026-03-09 13:07:59 +0100

    refactor: ResponseFintEventConsumer from java to kotlin

--- src/main/java/no/fintlabs/provider/event/response/ResponseFintEventConsumer.kt
+++ src/main/java/no/fintlabs/provider/event/response/ResponseFintEventConsumer.kt
@@ -1,58 +1,58 @@
-package no.fintlabs.provider.event.response;
+package no.fintlabs.provider.event.response
 
-import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
-import no.fintlabs.adapter.models.event.ResponseFintEvent;
-import no.fintlabs.kafka.common.topic.pattern.FormattedTopicComponentPattern;
-import no.fintlabs.kafka.common.topic.pattern.ValidatedTopicComponentPattern;
-import no.fintlabs.kafka.event.EventConsumerConfiguration;
-import no.fintlabs.kafka.event.EventConsumerFactoryService;
-import no.fintlabs.kafka.event.topic.EventTopicNamePatternParameters;
-import no.fintlabs.provider.config.KafkaConfig;
-import no.fintlabs.provider.event.request.RequestEventService;
-import no.novari.metamodel.MetamodelService;
-import org.apache.kafka.clients.consumer.ConsumerRecord;
-import org.springframework.context.annotation.Bean;
-import org.springframework.kafka.listener.ConcurrentMessageListenerContainer;
-import org.springframework.stereotype.Service;
+import no.fintlabs.adapter.models.event.ResponseFintEvent
+import no.fintlabs.kafka.common.topic.pattern.FormattedTopicComponentPattern
+import no.fintlabs.kafka.common.topic.pattern.ValidatedTopicComponentPattern
+import no.fintlabs.kafka.event.EventConsumerConfiguration
+import no.fintlabs.kafka.event.EventConsumerFactoryService
+import no.fintlabs.kafka.event.topic.EventTopicNamePatternParameters
+import no.fintlabs.provider.config.KafkaConfig
+import no.fintlabs.provider.event.request.RequestEventService
+import no.novari.metamodel.MetamodelService
+import org.apache.kafka.clients.consumer.ConsumerRecord
+import org.slf4j.LoggerFactory
+import org.springframework.context.annotation.Bean
+import org.springframework.kafka.listener.ConcurrentMessageListenerContainer
+import org.springframework.stereotype.Service
 
-@Slf4j
 @Service
-@RequiredArgsConstructor
-public class ResponseFintEventConsumer {
+class ResponseFintEventConsumer(
+    private val eventConsumerFactoryService: EventConsumerFactoryService,
+    private val requestEventService: RequestEventService,
+    private val metamodelService: MetamodelService,
+    private val kafkaConfig: KafkaConfig,
+) {
 
-    private final EventConsumerFactoryService eventConsumerFactoryService;
-    private final RequestEventService requestEventService;
-    private final MetamodelService metamodelService;
-    private final KafkaConfig kafkaConfig;
+    private val logger = LoggerFactory.getLogger(javaClass)
 
     @Bean
-    public ConcurrentMessageListenerContainer<String, ResponseFintEvent> registerResponseFintEventListener() {
-        return eventConsumerFactoryService.createFactory(
-                ResponseFintEvent.class,
-                this::processEvent,
-                EventConsumerConfiguration
-                        .builder()
-                        .seekingOffsetResetOnAssignment(true)
-                        .groupIdSuffix(kafkaConfig.getGroupIdSuffix())
-                        .build()
+    fun registerResponseFintEventListener(): ConcurrentMessageListenerContainer<String?, ResponseFintEvent> =
+        eventConsumerFactoryService.createFactory(
+            ResponseFintEvent::class.java,
+            this::processEvent,
+            EventConsumerConfiguration
+                .builder()
+                .seekingOffsetResetOnAssignment(true)
+                .groupIdSuffix(kafkaConfig.groupIdSuffix)
+                .build()
         ).createContainer(
-                EventTopicNamePatternParameters
-                        .builder()
-                        .orgId(FormattedTopicComponentPattern.any())
-                        .domainContext(FormattedTopicComponentPattern.anyOf("fint-core"))
-                        .eventName(ValidatedTopicComponentPattern.anyOf(createEventNames()))
-                        .build()
-        );
-    }
+            EventTopicNamePatternParameters
+                .builder()
+                .orgId(FormattedTopicComponentPattern.any())
+                .domainContext(FormattedTopicComponentPattern.anyOf("fint-core"))
+                .eventName(ValidatedTopicComponentPattern.anyOf(*createEventNames()))
+                .build()
+        )
 
-    private String[] createEventNames() {
-        return metamodelService.getResources().stream().map(resource -> resource.getName() + "-response").toArray(String[]::new);
-    }
+    private fun createEventNames(): Array<String> =
+        metamodelService.getComponents().flatMap { component ->
+            component.resources.map { resource ->
+                "${component.domainName}-${component.packageName}-${resource.name}-response"
+            }
+        }.toTypedArray()
 
-    private void processEvent(ConsumerRecord<String, ResponseFintEvent> consumerRecord) {
-        log.info("ResponseFintEvent received: {} - {}", consumerRecord.value().getOrgId(), consumerRecord.value().getCorrId());
-        requestEventService.removeEvent(consumerRecord.value().getCorrId());
+    private fun processEvent(consumerRecord: ConsumerRecord<String?, ResponseFintEvent>) {
+        logger.info("ResponseFintEvent received: {} - {}", consumerRecord.value().orgId, consumerRecord.value().corrId)
+        requestEventService.removeEvent(consumerRecord.value().corrId)
     }
-
 }
