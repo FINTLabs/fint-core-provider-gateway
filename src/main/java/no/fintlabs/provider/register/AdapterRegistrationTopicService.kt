43514ad44de267bddbf2b390f291f59e5f48f25e
Hknots <hknots99@gmail.com>
2026-03-10 10:05:37 +0100

    refactor: use EntityKafkaProperties for partitions and retention time in AdapterRegistrationTopicService

--- src/main/java/no/fintlabs/provider/register/AdapterRegistrationTopicService.kt
+++ src/main/java/no/fintlabs/provider/register/AdapterRegistrationTopicService.kt
@@ -2,33 +2,31 @@ package no.fintlabs.provider.register
 
 import no.fintlabs.adapter.models.AdapterCapability
 import no.fintlabs.adapter.models.AdapterContract
-import no.fintlabs.provider.kafka.TopicNamesConstants
+import no.fintlabs.provider.config.EntityKafkaProperties
 import no.novari.kafka.topic.EntityTopicService
 import no.novari.kafka.topic.configuration.EntityCleanupFrequency
 import no.novari.kafka.topic.configuration.EntityTopicConfiguration
 import no.novari.kafka.topic.name.EntityTopicNameParameters
 import no.novari.kafka.topic.name.TopicNamePrefixParameters
 import org.springframework.stereotype.Service
-import java.time.Duration
 import java.util.function.Consumer
 
 @Service
 class AdapterRegistrationTopicService(
-    private val entityTopicService: EntityTopicService
+    private val entityTopicService: EntityTopicService,
+    private val entityKafkaProperties: EntityKafkaProperties,
 ) {
 
     fun ensureCapabilityTopics(adapterContract: AdapterContract) {
         adapterContract.capabilities.forEach(Consumer { capability: AdapterCapability ->
             // TODO: Change retention time to be based on capability (Verify that Visma agrees with the latest contract)
-            val retentionTime = Duration.ofDays(7)
-
             entityTopicService.createOrModifyTopic(
                 createTopicNameParameters(adapterContract.orgId, capability),
                 EntityTopicConfiguration
                     .stepBuilder()
-                    .partitions(1)
-                    .lastValueRetentionTime(retentionTime)
-                    .nullValueRetentionTime(retentionTime)
+                    .partitions(entityKafkaProperties.partitions)
+                    .lastValueRetentionTime(entityKafkaProperties.retentionTime)
+                    .nullValueRetentionTime(entityKafkaProperties.retentionTime)
                     .cleanupFrequency(EntityCleanupFrequency.NORMAL)
                     .build()
             )
