6ed4c25bf6f036e9b5f8751a5baa5468f525d1e8
Henrik Knutsen <hknots99@gmail.com>
2026-03-12 14:16:04 +0100

    Fix/only create topics once (#60)
    * refactor: rename method to reflect single-purpose functionality
    
    * refactor: introduce topic existence check to prevent redundant creation

--- src/main/java/no/fintlabs/provider/register/RegistrationService.java
+++ src/main/java/no/fintlabs/provider/register/RegistrationService.java
@@ -20,7 +20,7 @@ public class RegistrationService {
     public void register(AdapterContract adapterContract) {
         adapterRegistrationValidator.validateCapabilities(adapterContract.getCapabilities());
         adapterContract.setTime(System.currentTimeMillis());
-        adapterRegistrationTopicService.createOrModifyCapabilityTopics(adapterContract);
+        adapterRegistrationTopicService.createCapabilityTopics(adapterContract);
         adapterContractProducer.send(adapterContract);
         adapterContractContext.add(adapterContract);
         log.info("New adapter has registered: {}", adapterContract.getAdapterId());
