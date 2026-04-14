8f24a7bd0d4d0bf87c7c32b05d900d7efe68f423
Hknots <hknots99@gmail.com>
2026-04-14 15:59:22 +0200

    feat: add ContractService.kt

---
+++ src/main/java/no/fintlabs/provider/register/ContractService.kt
@@ -0,0 +1,43 @@
+package no.fintlabs.provider.register
+
+import no.fintlabs.adapter.models.AdapterContract
+import org.slf4j.LoggerFactory
+import org.springframework.stereotype.Service
+
+@Service
+class ContractService(
+    private val contractJpaRepository: ContractJpaRepository
+) {
+    private val log = LoggerFactory.getLogger(javaClass)
+
+    fun getAdapterIds(): Set<String> = contractJpaRepository.getAdapterIds()
+
+    fun saveContract(adapterContract: AdapterContract) {
+        contractJpaRepository.save(ContractEntity(adapterContract))
+        log.info("AdapterContract saved: {}", adapterContract.adapterId)
+    }
+
+    /**
+     * @return `true` if capability matches, `false` if not, `null` if contract not found.
+     */
+    fun adapterCanPerformCapability(
+        adapterId: String,
+        domainName: String,
+        packageName: String,
+        entityName: String
+    ): Boolean? =
+        contractJpaRepository.findByAdapterIdWithCapabilities(adapterId)
+            .map { contract -> contract.capabilityEntityset.any { it.matches(domainName, packageName, entityName) } }
+            .orElse(null)
+
+    /**
+     * @return `true` if username matches, `false` if not, `null` if contract not found.
+     */
+    fun userCanAccessAdapter(username: String, adapterId: String): Boolean? =
+        contractJpaRepository.findById(adapterId)
+            .map { it.username == username }
+            .orElse(null)
+
+    private fun CapabilityEntity.matches(domainName: String, packageName: String, resourceName: String): Boolean =
+        this.domainName == domainName && this.pkgName == packageName && this.resourceName == resourceName
+}
